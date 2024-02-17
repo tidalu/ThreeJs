c111b4910d993291206b21d2ad4f010233c7f114
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-02-17 19:11:34 +0100

    created basic scene

--- Course/03-first-threejs-project/src/script.js
+++ Course/03-first-threejs-project/src/script.js
@@ -1,3 +1,36 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+const canvas = document.querySelector('canvas.webgl');
+// Sceene
+const scene = new THREE.Scene();
+
+// Object
+const myGeometry = new THREE.BoxGeometry(1, 1, 1); // width , height and depth,
+const material = new THREE.MeshBasicMaterial({
+  color: 'yellow',
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(myGeometry, material);
+
+scene.add(mesh);
+
+// camera
+// Lets start with the PerspectiveCamera class, Two essential paramters: field of view and aspect ratio
+let sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
