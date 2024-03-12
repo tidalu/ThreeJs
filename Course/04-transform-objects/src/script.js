620a8f5ad770f98c5b11878de0b3d5e1554da506
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-03-12 11:09:03 +0100

    learned rotating and grouping meshes

--- Course/04-transform-objects/src/script.js
+++ Course/04-transform-objects/src/script.js
@@ -4,22 +4,43 @@ const canvas = document.querySelector('canvas.webgl');
 
 const scene = new THREE.Scene();
 
-const object = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 'lightblue', wireframe: true })
-);
-// object.position.x = 0.7;
-// object.position.y = -0.6;
+const group = new THREE.Group()
+scene.add(group)
 
-// object.position.set(0.7, -0.9, 0);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1,1 ), 
+  new THREE.MeshBasicMaterial({
+    color: 'purple', 
+    wireframe: true
+  })
+)
 
-object.scale.x = 1;
-object.scale.y = 1;
-object.scale.z = 1;
-scene.add(object);
+cube1.position.x = -2
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1,1 ), 
+  new THREE.MeshBasicMaterial({
+    color: 'blue', 
+    wireframe: true
+  })
+)
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1,1 ), 
+  new THREE.MeshBasicMaterial({
+    color: 'green', 
+    wireframe: true
+  })
+) 
+cube3.position.x = 2
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+group.scale.set(0.7, 0.7, 0.7)
+
 
 const axesHelper = new THREE.AxesHelper();
-// scene.add(axesHelper);
+scene.add(axesHelper);
 
 const sizes = {
   width: 800,
@@ -30,17 +51,24 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 
 camera.position.z = 3;
 scene.add(camera);
+// camera.lookAt(object.position)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-
+renderer.render(scene, camera)
 function animate() {
   requestAnimationFrame(animate);
-  object.rotation.x += 0.01;
-  object.rotation.y += 0.01;
+  cube1.rotation.x += 0.01;
+  cube1.rotation.y += 0.01;
+  cube2.rotation.x += 0.01;
+  cube2.rotation.y += 0.01;
+  cube3.rotation.x += 0.01;
+  cube3.rotation.y += 0.01;
+  group.rotation.x += 0.01;
+  group.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
