b3cabe8a8a4924919bb066883b3b3f7aa9734abf
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-02 15:17:09 +0200

    set up project, set up scene, add objects, animate object, particles, add parallax effect

--- Course/19-scroll-based-animation/src/script.js
+++ Course/19-scroll-based-animation/src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import GUI from 'lil-gui';
-
+import gsap from 'gsap';
 /**
  * Debug
  */
@@ -39,6 +39,7 @@ const material = new THREE.MeshToonMaterial({
 
 gui.addColor(parameters, 'materialColor').onChange(() => {
   material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
 });
 
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 70), material);
@@ -59,6 +60,33 @@ scene.add(mesh1, mesh2, mesh3);
 
 const sectionMeshes = [mesh1, mesh2, mesh3];
 
+// Particles
+
+const count = 400;
+const positions = new Float32Array(count * 3);
+
+for (let i = 0; i < count; i++) {
+  positions[i * 3] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.02,
+});
+
+const particle = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particle);
 // lights
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
@@ -115,9 +143,24 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // scroll section
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener('scroll', () => {
   scrollY = window.scrollY;
+
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection !== currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: 'power2.inOut',
+      x: '+=6',
+      y: '+=3',
+      z: '+=1/5',
+    });
+  }
 });
 
 // cursor
@@ -140,12 +183,11 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - prevTime;
   prevTime = elapsedTime;
-  console.log(deltaTime);
 
   // update objects
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.5;
-    mesh.rotation.y = elapsedTime * 0.5;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // animate camera
